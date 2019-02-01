1a6309d53d218d00dcf01f79de33531c948a750d
Qeed1o <sergeykavai@gmail.com>
2019-02-01 02:53:35 +0200

    partially done fog + rewiewed some code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var
-	size_W = 15, size_H = 10, main_EL,
+	size_W = 15, size_H = 10, main_EL, counter = 0,
 	level = ["D","D","D","D","D","D","D","D","D","D","D","D","D","D","M",
 			"D","D","D","D","D","D","D","D","D","D","D","D","D","D","D",
 			"D","D","D","D","D","D","D","D","D","D","D","D","D","D","D",
@@ -14,18 +14,77 @@ var
 
 function InitLevel(){
 	main_EL = document.getElementById("game_MainWrapper");
-	for(let i=0; i<size_W * size_H; i++){
-		let
-			div = document.createElement('div'),
-			className;
+	main_EL.innerHTML = "";
 
-		level[i] == "D" ? className = "game_default" : {};
-		level[i] == "W" ? className = "game_wall" : {};
-		level[i] == "N" ? className = "game_nextLevel" : {};
-		level[i] == "M" ? className = "game_mob" : {};
-		//console.log(level[i]);
+	for(let i = 0; i < size_H; i++){
+		for(let j = 0; j < size_W; j++){
+			let
+				div = document.createElement('div'),
+				className;
+			div.setAttribute("Col",j);
+			div.setAttribute("Row",i);
 
-		div.className = className;
-		main_EL.appendChild(div);
+			if(level[counter] == "M"){ // Монстр
+				className = "game_mob";
+			}
+			else if(level[counter] == "W"){ // Стена
+				className = "game_wall";
+			}
+			else if(level[counter] == "N"){ // Следующий уровень
+				className = "game_nextLevel";
+			}
+			else{ // Обычный "блок"
+				className = "game_default";
+				div.onclick = () => click_Default(div);
+			}
+			//console.log(level[i]);
+
+			div.className = className;
+			(i == 0 && j == 0) ||
+			(i == 1 && j == 0) ||
+			(i == 0 && j == 1) ? {} : div.classList.add("fogged");
+			main_EL.appendChild(div);
+			counter += 1;
+		}
+	}
+}
+
+function click_Default(el){ // Клик на обычный блок
+	if(el.classList.contains("fogged")){ // Не видим элемент
+		{}
+	}else{
+		clear_Fog(el.getAttribute("Col"),el.getAttribute("Row"));	
+	}
+}
+
+function clear_Fog(col,row){
+	for(let i = 0; i < main_EL.childNodes.length; i++){
+		if(main_EL.childNodes[i].getAttribute("Col") == col
+							&&
+			main_EL.childNodes[i].getAttribute("Row") == row){
+				if(col == 0){
+					let child = main_EL.childNodes;
+					main_EL.childNodes[i+1].classList.remove("fogged");
+				}else if(col == 14){
+					let child = main_EL.childNodes;
+					main_EL.childNodes[i-1].classList.remove("fogged");
+				}else{
+					let child = main_EL.childNodes;
+					main_EL.childNodes[i+1].classList.remove("fogged");
+					main_EL.childNodes[i-1].classList.remove("fogged");
+				}
+
+				if(row == 0){
+					let child = main_EL.childNodes;
+					main_EL.childNodes[i+15].classList.remove("fogged");
+				}else if(row == 9){
+					let child = main_EL.childNodes;
+					main_EL.childNodes[i-15].classList.remove("fogged");
+				}else{
+					let child = main_EL.childNodes;
+					main_EL.childNodes[i-15].classList.remove("fogged");
+					main_EL.childNodes[i+15].classList.remove("fogged");
+				}
+		}
 	}
 }
